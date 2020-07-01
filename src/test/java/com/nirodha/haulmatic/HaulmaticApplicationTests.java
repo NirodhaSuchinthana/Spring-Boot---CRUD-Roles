7db6d02166621c6acf92a4d7e4d5b467809e1799
Nirodha Suchinthana <suchithanaapn@gmail.com>
2020-07-01 09:28:40 +0530

    initial commit

---
+++ src/test/java/com/nirodha/haulmatic/HaulmaticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nirodha.haulmatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaulmaticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
