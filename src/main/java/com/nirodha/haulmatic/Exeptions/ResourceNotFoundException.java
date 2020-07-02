2f4c33b82b512a635ab11f86ecf09a1f59c563c2
Nirodha Suchinthana <suchithanaapn@gmail.com>
2020-07-02 18:24:56 +0530

    Integration test for read is implemented

--- src/main/java/com/nirodha/haulmatic/Exeptions/ResourceNotFoundException.java
+++ src/main/java/com/nirodha/haulmatic/Exeptions/ResourceNotFoundException.java
@@ -1,13 +1,13 @@
-package com.nirodha.haulmatic.repository;
+package com.nirodha.haulmatic.Exeptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends Exception {
     private static final long serialVersionUID = 1L;
     public ResourceNotFoundException(String message){
-        super();
+        super(message);
     }
 
 }
--- src/test/java/com/nirodha/haulmatic/HaulmaticApplicationTests.java
+++ src/test/java/com/nirodha/haulmatic/HaulmaticApplicationTests.java
@@ -1,13 +1,48 @@
 package com.nirodha.haulmatic;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-@SpringBootTest
+import org.springframework.util.MultiValueMap;
+import org.testng.annotations.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.HttpClientErrorException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = HaulmaticApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HaulmaticApplicationTests {
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+
+	@LocalServerPort
+	private int port;
+
+	private String getRootUrl() {
+		return "http://localhost:" + port;
+	}
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	public void getAllRoles(){
+		HttpHeaders headers = new HttpHeaders();
+		HttpEntity<String> entity = new HttpEntity<String>(null,headers);
+
+		ResponseEntity<String> responseEntity = testRestTemplate.exchange(getRootUrl()+"/roles", HttpMethod.GET, entity, String.class);
+		assertNotNull(responseEntity.getBody());
+	}
+
 
 }
