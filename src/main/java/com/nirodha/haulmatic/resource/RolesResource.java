899b9750b42dbbc4110e626a5ec13c64e69011e8
Nirodha Suchinthana <suchithanaapn@gmail.com>
2020-07-02 09:59:10 +0530

    create,update and read implemented

--- src/main/java/com/nirodha/haulmatic/resource/RolesResource.java
+++ src/main/java/com/nirodha/haulmatic/resource/RolesResource.java
@@ -2,11 +2,12 @@ package com.nirodha.haulmatic.resource;
 
 import com.nirodha.haulmatic.documents.Roles;
 import com.nirodha.haulmatic.repository.RolesRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/roles")
@@ -18,8 +19,37 @@ public class RolesResource {
         this.rolesRepository = rolesRepository;
     }
 
+    @PostMapping("/")
+    public ResponseEntity<Roles> createRole(Roles roles){
+        try {
+            Roles _role = rolesRepository.save(new Roles(roles.getId(), roles.getOrganization(), roles.getFirstName(), roles.getLastName(),roles.getNicNo(), roles.getRoleType()));
+            return new ResponseEntity<>(_role, HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
     @GetMapping("/all")
     public List<Roles> getAll(){
         return rolesRepository.findAll();
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Roles> updateRole(@PathVariable("id") String id, @RequestBody Roles roleUpdate ){
+        Optional<Roles> role = rolesRepository.findById(Integer.valueOf(id));
+
+        if (role.isPresent()){
+            Roles _role = role.get();
+
+            _role.setOrganization(roleUpdate.getOrganization());
+            _role.setFirstName(roleUpdate.getFirstName());
+            _role.setLastName(roleUpdate.getLastName());
+            _role.setNicNo(roleUpdate.getNicNo());
+            _role.setRoleType(roleUpdate.getRoleType());
+
+            return new ResponseEntity<>(rolesRepository.save(_role), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
